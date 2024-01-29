33301f0a3d002de3ec4fe7558a2107ff6c2261a5
notnickle <thatnickle@gmail.com>
2024-01-29 17:26:01 -0500

    mergeTwoLists problem logged

--- src/Library/linked_list_prob.py
+++ src/Library/linked_list_prob.py
@@ -4,6 +4,13 @@ def addTwoNumbers(self , l1, l2):
     return
 
 
+def mergeTwoLists(self, list1, list2):
+
+    return
+
+
+
+
 def linkedListProbSelect():
     print("Please select from the list of problems: \n")
     
